82d18a895ac18721ea9a73d1130847db7a58f224
zfdcode <zfdstudy@outlook.com>
2017-11-20 20:01:43 +0100

    Updated docs

--- HotpotFund/HotpotFund.py
+++ HotpotFund/HotpotFund.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import time
+import os
 
 
 class HotpotFund:
-    def __init__(self, benchmark_path="benchmark/", csv_delimiter=","):
+    def __init__(self, benchmark_path="benchmark/", csv_delimiter=",", record_name="record.csv", label_name="label.csv"):
+        self.record_name = record_name
+        self.label_name = label_name
+        self.benchmark_path = benchmark_path
         records = self.read_file(
-            benchmark_path, "record.csv", csv_delimiter)
+            benchmark_path, record_name, csv_delimiter)
         labels = self.read_file(
-            benchmark_path, "label.csv", csv_delimiter)
+            benchmark_path, label_name, csv_delimiter)
         self.records = self.read_records(records)
         self.labels = self.read_labels(labels)
 
@@ -43,24 +47,50 @@ class HotpotFund:
         labels_string_list = []
         for index, label in self.labels.items():
             if label.parent != None:
+                labels_string_list.append(
+                    ":".join([str(index), label.name, label.parent.name]))
+            else:
                 labels_string_list.append(":".join([str(index), label.name]))
         return labels_string_list
 
     def creat_new_label(self, index):
-        name = input("Type the name of label")
-        parent_index = int(input("Type the parent index of label\n"))
+        name = input("Type the name of label ")
+        parent_index = int(input("Type the parent index of label "))
         while parent_index not in self.labels.keys() and parent_index != index:
-            parent_index = int(input("Type the parent index of label\n"))
-        self.labels[index] = Label(
+            parent_index = int(input("Type the parent index of label "))
+        new_label = Label(
             index, name, None if parent_index == index else self.labels[parent_index])
+        self.labels[index] = new_label
+        self.write_file(self.benchmark_path + self.label_name,
+                        ",".join([str(index), str(parent_index), name]) + "\n")
 
     def start_fund(self):
-        selected_label_index = input(
-            "Select the index:\n" + '\n'.join(self.get_labels()) + "\n")
-        if int(selected_label_index) not in self.labels.keys():
+        selected_label_index = int(input(
+            "Select the index:\n" + '\n'.join(self.get_labels()) + "\n"))
+        if selected_label_index not in self.labels.keys():
             self.creat_new_label(selected_label_index)
+        self.selected_label_index = selected_label_index
         self.start_time = time.time()
         self.stop_time = time.time()
+
+    def stop_fund(self):
+        self.stop_time = time.time()
+        self.write_file(self.benchmark_path + self.record_name,
+                        ",".join([str(self.start_time), str(self.stop_time), str(self.selected_label_index)]) + "\n")
+
+    def write_file(self, file_path, csv_lines):
+        if os.path.exists(file_path):
+            append_write = 'a'  # append if already exists
+        else:
+            append_write = 'w'  # make a new file if not
+        file = open(file_path, append_write)
+        if isinstance(csv_lines, list):
+            for csv_line in csv_lines:
+                file.write(csv_line)
+        else:
+            file.write(csv_lines)
+            print(csv_lines)
+        file.close()
 
 
 class Label:
